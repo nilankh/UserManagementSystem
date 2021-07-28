505f492bf9aa960d238af2753477d93d05893523
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-28 12:06:43 +0530

    Created first ejs file

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const morgan = require('morgan');
 const app = express();
 
+dotenv.config({ path: 'config.env' });
+const PORT = process.PORT || 8000;
+
+// Log requests
+app.use(morgan('tiny'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('assets'));
+
+
+// Set view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-    res.send('User management Application')
+  return res.render('index')
 });
 
-
-app.listen(3000, () => {
-    console.log('App listening on port 3000!');
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
