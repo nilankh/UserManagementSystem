4a623b93d1e6cea0fbe543451ad411b6df942c00
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-30 14:56:53 +0530

    Update api working

--- controllers/controller.js
+++ controllers/controller.js
@@ -41,7 +41,25 @@ exports.find = (req, res) => {
 };
 
 // Update a new identified user by user id
-exports.update = (req, res) => {};
+exports.update = (req, res) => {
+    if(!req.body){
+        return res
+            .status(400)
+            .send({message: "Data to update can not be empty"})
+    }
+    const id = req.params.id;
+    UserDB.findByIdAndUpdate(id, req.body, {useFindAndModify: false})
+        .then(data => {
+            if(!data){
+                res.status(404).send({message: `Cannot Update user with ${id}, Maybe user not found`})
+            }else{
+                res.send(data);
+            }
+        })
+        .catch(err => {
+            res.status(500).send({message: "Error in updating user information"})
+        })
+};
 
 // Delete a user with specified user id
 // create and sava new user
