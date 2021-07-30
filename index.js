dc675d7ed386d5c282e9910f9d77ace976f19517
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-30 13:40:34 +0530

    skjd

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const path = require('path');
 const morgan = require('morgan');
 const app = express();
 
+
+const connectDB = require('./config/mongoose')
+
 dotenv.config({ path: 'config.env' });
+console.log(process.env)
 const PORT = process.PORT || 8000;
 
 // Log requests
 app.use(morgan('tiny'));
+
+// MongoDB Connection
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- routes/router.js
+++ routes/router.js
@@ -10,8 +10,17 @@ const services = require('../controllers/render')
 */
 route.get('/',services.homeRoutes);
 
+/*
+    @description add users
+    @method GET/add-user
+*/
 route.get('/add-user', services.add_user);
 
+
+/*
+    @description update-user
+    @method POST/update-user
+*/
 route.get('/update-user', services.update_user);
 
 module.exports = route
