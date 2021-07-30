12c5c8be0a2af87f0e471b141733fbc53045fbdc
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-07-30 14:30:17 +0530

    Done with create user

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,8 +2,31 @@ const UserDB = require('../model/model');
 
 // create and sava new user
 exports.create = (req, res) => {
-    // validate request
-    
+  // validate request
+  if (!req.body) {
+    res.status(400).send({ message: 'Content can not be empty!' });
+    return;
+  }
+  // New user
+  const user = new UserDB({
+    name: req.body.name,
+    email: req.body.email,
+    gender: req.body.gender,
+    status: req.body.status,
+  });
+
+  // Save user in the db
+  user
+    .save(user)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || 'Some error occured while creating a create operation',
+      });
+    });
 };
 
 // retrieve and return all users/retrieve and return a single user
