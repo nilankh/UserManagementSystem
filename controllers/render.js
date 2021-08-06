5a1f522c7e6b26efeac5af2017cf62a361e30701
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-08-06 11:12:32 +0530

    using the api and displaying dynamic data

--- controllers/render.js
+++ controllers/render.js
@@ -1,13 +1,21 @@
-
+const axios = require('axios');
 
 exports.homeRoutes = (req, res) => {
-    return res.render('index');
-}
+  // Make a get request to /api/users
+  axios.get('http://localhost:8000/api/users')
+  .then(function(response) {
+    // console.log(response)  
+    res.render('index', { users: response.data });
+  })
+  .catch(err => {
+      res.send(err)
+  })
+};
 
 exports.add_user = (req, res) => {
-    return res.render('add_user');
-}
+  return res.render('add_user');
+};
 
 exports.update_user = (req, res) => {
-     return res.render('update_user');
-}
+  return res.render('update_user');
+};
